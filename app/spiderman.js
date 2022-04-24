932d351fb4740c25af824067aeaaa9ceecfbccd4
hisahito <hisahito706@gmail.com>
2022-04-23 23:43:37 -0500

    agregando segundo commit y segunda prueba. Test passed. Se imporo modulo de la clase en la prueba

---
+++ app/spiderman.js
@@ -0,0 +1,14 @@
+// Creando una clase spiderman con datos para luego probarlos
+
+
+class spiderman {
+    constructor(name, age, actor, movies, studio) {
+        this.name = name,
+        this.age = age,
+        this.actor = actor,
+        this.movies = [movies],
+        this.studio = studio
+    }
+}
+
+module.exports = spiderman;
--- test/spiderman.test.js
+++ test/spiderman.test.js
@@ -1,8 +1,10 @@
 // Prueba 1 del ejercicio 2 de la weekly mission 3
 
-describe("Test Suite Dummy Description", () => {
-    test('Case 1 Dummy', () => {
-      const resultOfSomething = 1 + 2
-      expect(resultOfSomething).toBe(18);
+const Spiderman = require('./../app/spiderman');
+
+describe("unit test for spiderman class", () => {
+    test('1) create an spiderman object', () => {
+      const tobbyMaguire = new Spiderman('Peter Parker', 45, 'Tobias Vincent Maguire', ['Spiderman', 'Spiderman 2', 'Spiderman 3'], 'Sony');
+      expect(tobbyMaguire.name).toBe('Peter Parker');
     });
   })
