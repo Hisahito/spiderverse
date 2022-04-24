361c54f168d0126e72163b2723c83b501cce8cc1
hisahito <hisahito706@gmail.com>
2022-04-23 23:16:08 -0500

    inicializando primer test, estructura de archivos y primer commit

---
+++ test/spiderman.test.js
@@ -0,0 +1,8 @@
+// Prueba 1 del ejercicio 2 de la weekly mission 3
+
+describe("Test Suite Dummy Description", () => {
+    test('Case 1 Dummy', () => {
+      const resultOfSomething = 1 + 2
+      expect(resultOfSomething).toBe(18);
+    });
+  })
